a117a8dea0dd42cd80f5fed7ecbe6c3a343e7e92
Taek Jin Han <tj.han@yale.edu>
2021-06-29 09:29:58 -0400

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World!"
+      end 
+
+    get '/name' do 
+        "My name is Victoria."
+    end
+    
+    get '/hometown' do 
+        "My hometown is Chrlottesville"
+    end
+
+    get '/favorite-song' do 
+        "My favorite song is Shake It Off" 
+    end
+
 end
